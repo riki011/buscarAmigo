0f62aacfa4df1709582deab65ef5a26b303818cc
Ricardo Genaizir <ricardogenaizir1@gmail.com>
2021-11-19 15:38:24 -0300

    asd

--- src/main/java/com/example/demo/controller/AmigoInvisibleController.java
+++ src/main/java/com/example/demo/controller/AmigoInvisibleController.java
@@ -8,6 +8,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class AmigoInvisibleController {
 	
 	private List<String> lista = new ArrayList<>();
