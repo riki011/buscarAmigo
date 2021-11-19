d0ea2e5811f9f7afa80dacf6e90739de03f015c2
Ricardo Genaizir <ricardogenaizir1@gmail.com>
2021-11-19 15:35:41 -0300

    asd

--- src/main/java/com/example/demo/controller/AmigoInvisibleController.java
+++ src/main/java/com/example/demo/controller/AmigoInvisibleController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("buscar")
 public class AmigoInvisibleController {
 	
 	private List<String> lista = new ArrayList<>();
---
+++ src/main/java/com/example/demo/controller/WebSecurityConfig.java
@@ -0,0 +1,38 @@
+package com.example.demo.controller;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@EnableWebSecurity
+public class WebSecurityConfig  extends WebSecurityConfigurerAdapter{
+	
+	@Override
+	protected void configure(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity.csrf().disable()
+		.authorizeRequests().antMatchers("/**").permitAll().
+				anyRequest().authenticated()
+				.and().cors().and()
+				.exceptionHandling();
+	}
+
+	@Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        //config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+}
